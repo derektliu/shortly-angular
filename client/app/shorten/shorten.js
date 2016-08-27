f766bab676525afa8dd843da9f17de3266249c45
Derek Liu <liu.derekt@gmail.com>
2016-08-27 11:17:33 -0700

    Pass spec tests.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,13 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    Links.addOne($scope.newLink)
+    .then(function(resp) {
+      $scope.link[$scope.newLink] = $scope.newLink;
+      $scope.newLink = '';
+    });
+  };
 });
