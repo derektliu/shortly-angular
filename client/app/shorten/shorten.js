2bd3781c58f4e54cd040741bf8614ccad5fd1e5c
Derek Liu <liu.derekt@gmail.com>
2016-08-27 16:07:31 -0700

    Add form validation to shorten input.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,13 +3,17 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
 
-  $scope.addLink = function() {
-    
-    $scope.link.url = $scope.newLink;
-
-    Links.addOne($scope.link)
-    .then(function(resp) {
-      $scope.newLink = '';
-    });
+  $scope.addLink = function(isValid) {
+    console.log('Valid?', isValid);
+    if (isValid) {
+      $scope.link.url = $scope.newLink;
+      
+      Links.addOne($scope.link)
+      .then(function(resp) {
+        $scope.newLink = '';
+      });
+    } else {
+      console.log('Link is not valid');
+    }
   };
 });
