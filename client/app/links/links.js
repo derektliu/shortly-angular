8641c791521483f48899e7429e5da2d945edd401
Derek Liu <liu.derekt@gmail.com>
2016-08-27 17:34:21 -0700

    add filtering and sorting for links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,9 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links, Auth) {
   $scope.data = {};
+  $scope.sortType = 'title';
+  $scope.sortReverse = false;
+  $scope.searchLink = '';
 
   Links.getAll()
   .then(function(links) {
